32c0ec03f69b5c504948aa66294fa7cf6a637fe1
Linux After Dark <alex.wagner1883@gmail.com>
2026-09-08 21:27:34 -0700

    Create setup.sh

---
+++ capture-the-flag/01-hidden-in-plain-sight/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p "$HOME/.archive"
+
+echo "LaD{hidden_files_are_not_secret}" > "$HOME/.archive/access.txt"
+
+echo "backup files" > "$HOME/notes.txt"
+echo "nothing interesting here" > "$HOME/readme.txt"
